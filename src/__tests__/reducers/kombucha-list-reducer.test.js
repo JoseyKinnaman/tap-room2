2f7c630f6fac60e29c7d8a94622723389e87739c
Jozy Kinnaman <jozypants@gmail.com>
2020-07-10 09:07:32 -0700

    add test for deleting a kombucha form the list. Expect to fail.

--- src/__tests__/reducers/kombucha-list-reducer.test.js
+++ src/__tests__/reducers/kombucha-list-reducer.test.js
@@ -1,6 +1,27 @@
 import kombuchaListReducer from '../../reducers/kombucha-list-reducer';
 
 describe ('kombuchaListReducer', () => {
+
+  const currentState ={
+    1: {
+      name: "Purple Rain",
+      brand: "Electric",
+      price: 56,
+      alcoholContent: 5.9,
+      flavor: "Grape",
+      pints: "124",
+      id: 1
+    },
+    2: {
+      name: "The Red in the Sky is Ours",
+      brand: "ATG",
+      price: 59,
+      alcoholContent: 6,
+      flavor: "Blood Orange",
+      pints: "124",
+      id: 2
+    }
+  }
   let action;
   const kombuchaInfo = {
     name: "Purple Rain",
@@ -35,6 +56,23 @@ describe ('kombuchaListReducer', () => {
         flavor: flavor,
         pints: pints,
         id: id
+      }
+    });
+  });
+  test ('Should succesfully delete a kombucha', ()=>{
+    action = {
+      type: 'DELETE_KOMBUCHA',
+      id: 1
+    }
+    expect(kombuchaListReducer(currentState, action)).toEqual({
+      2: {
+        name: "The Red in the Sky is Ours",
+        brand: "ATG",
+        price: 59,
+        alcoholContent: 6,
+        flavor: "Blood Orange",
+        pints: "124",
+        id: 2
       }
     });
   });
