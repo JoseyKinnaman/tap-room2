15cba84ff262f367315a09a3fbeb51a8864c3a1d
Jozy Kinnaman <jozypants@gmail.com>
2020-07-10 13:25:10 -0700

    add test for select kombucha reducer. Expect fail.

--- src/__tests__/reducers/selected-kombucha-reducer.test.js
+++ src/__tests__/reducers/selected-kombucha-reducer.test.js
@@ -1,7 +1,31 @@
 import selectedKombuchaReducer from '../../reducers/selected-kombucha-reducer';
 
+
 describe("selectedKombuchaReducer", ()=>{
   test ('Should return default state if no action type is recognized', () => {
     expect(selectedKombuchaReducer(false, { type:null})).toEqual(false);
+  });
+
+  test('Should select a kombucha based on selection', () => {
+    const action = {
+    type: c.SELECT_KOMBUCHA,
+      name: "Purple Rain",
+      brand: "Electric",
+      price: 56,
+      alcoholContent: 5.9,
+      flavor: "Grape",
+      pints: "124",
+      id: 1
+     }
+     store.dispatch(action);
+     expect(selectedKombuchaReducer(null, action)).toEqual({
+       name: "Purple Rain",
+       brand: "Electric",
+       price: 56,
+       alcoholContent: 5.9,
+       flavor: "Grape",
+       pints: "124",
+       id: 1
+    })
   });
 });
