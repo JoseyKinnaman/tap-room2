b3a7c36b5c913d2f4b5e448958e78feaaab8a4f6
Jozy Kinnaman <jozypants@gmail.com>
2020-07-10 14:05:29 -0700

    add SELECT_KOMBUCHA to the index.js and write test for root reducer. Testpass.

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,8 +1,9 @@
 import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
-import kombuchaListReducer from '../../reducers/kombucha-list-reducer'
+import kombuchaListReducer from '../../reducers/kombucha-list-reducer';
 import * as c from '../../actions/ActionTypes'
+import selectedKombuchaReducer from '../../reducers/selected-kombucha-reducer';
  
  let store = createStore(rootReducer);
 
@@ -11,7 +12,8 @@ describe("rootReducer", () => {
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
       masterKombuchaList: {},
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      selectedKombucha: null
     });
   });
   test('Check that initial state of kombuchaListReducer matches root reducer', () => {
@@ -35,5 +37,20 @@ describe("rootReducer", () => {
     }
     store.dispatch(action);
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+  
+  test('Check that SELECT_KOMBUCHA causes change in selectedKombuchaReducer and root reducer', () => {
+    const action = {
+      type: c.SELECT_KOMBUCHA,
+      name: "Purple Rain",
+      brand: "Electric",
+      price: 56,
+      alcoholContent: 5.9,
+      flavor: "Grape",
+      pints: "124",
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().selectedKombucha).toEqual(selectedKombuchaReducer(undefined, action));
   });
 });
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import formVisibleReducer from './form-visible-reducer';
 import kombuchaListReducer from './kombucha-list-reducer';
+import selectedKombuchaReducer from './selected-kombucha-reducer'
 import { combineReducers } from 'redux';
+
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  masterKombuchaList: kombuchaListReducer
+  masterKombuchaList: kombuchaListReducer,
+  selectedKombucha: selectedKombuchaReducer
 });
 
 export default rootReducer;
