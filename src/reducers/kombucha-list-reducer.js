e5de559600a23f8f154986f82dfb6a6a95f6cae6
Jozy Kinnaman <jozypants@gmail.com>
2020-07-10 08:58:41 -0700

    add reducer action for adding kombucha. Test now passing.

--- src/reducers/kombucha-list-reducer.js
+++ src/reducers/kombucha-list-reducer.js
@@ -1,3 +1,19 @@
 export default  (state = {}, action) => {
-  return state;
+  const { name, brand, price, alcoholContent, flavor, pints, id} = action;
+  switch (action.type) {
+    case 'ADD_KOMBUCHA':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          price: price,
+          alcoholContent: alcoholContent,
+          flavor: flavor,
+          pints: pints,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
