b079376870e82f14913613e3f89a8ba00e55e968
Jozy Kinnaman <jozypants@gmail.com>
2020-07-12 09:49:16 -0700

    add test for buying a pint. Test passed.

--- src/__tests__/reducers/kombucha-list-reducer.test.js
+++ src/__tests__/reducers/kombucha-list-reducer.test.js
@@ -10,7 +10,7 @@ describe ('kombuchaListReducer', () => {
       price: 56,
       alcoholContent: 5.9,
       flavor: "Grape",
-      pints: "124",
+      pints: 124,
       id: 1
     },
     2: {
@@ -19,7 +19,7 @@ describe ('kombuchaListReducer', () => {
       price: 59,
       alcoholContent: 6,
       flavor: "Blood Orange",
-      pints: "124",
+      pints: 124,
       id: 2
     }
   }
@@ -30,7 +30,7 @@ describe ('kombuchaListReducer', () => {
     price: 56,
     alcoholContent: 5.9,
     flavor: "Grape",
-    pints: "124",
+    pints: 124,
     id:1
   };
   test('Should return default state if there is no action type passed into the reducer', () => {
@@ -79,13 +79,30 @@ describe ('kombuchaListReducer', () => {
       }
     });
   });
-  
+
   test('Should let the user succesfully buy a pint', () => {
+    const {name, brand, price, alcoholContent, flavor, pints, id} = kombuchaInfo
+    const newPints = pints -1;
     action ={
       type: c.BUY_PINT,
-      id:1
+      id: 1,
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      pints: pints,
+      flavor: flavor
     }
     expect(kombuchaListReducer(currentState, action)).toEqual({
+      [id]:{
+        id: id,
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pints: newPints,
+        flavor: flavor
+      },
       1: {
         name: "Purple Rain",
         brand: "Electric",
@@ -94,6 +111,15 @@ describe ('kombuchaListReducer', () => {
         flavor: "Grape",
         pints: 123,
         id: 1
+      },
+      2: {
+        name: "The Red in the Sky is Ours",
+        brand: "ATG",
+        price: 59,
+        alcoholContent: 6,
+        flavor: "Blood Orange",
+        pints: 124,
+        id: 2
       }
     })
   })
